42f6dcfd21458aa4c1fd032e5ed06d1c19d0507e
Tom <treez.zhang@gmail.com>
2020-07-02 12:09:29 -0700

    more demos

---
+++ homework/lesson3/DEMO.py
@@ -0,0 +1,7 @@
+x = "15"
+y = int(x) ** 2
+print ('''
+Seanny is going to eat a hamburger
+seanny went to Burgerking
+Seanny found a lettuce in his burger
+''')
---
+++ homework/lesson4/DEMO.py
@@ -0,0 +1,9 @@
+z = True
+x = int(input("Insert a number "))
+y = int(input("Another number: "))
+if x == 1 and y == 1 and z == True:
+    print("Hello")
+elif x == 2 or y != 2:
+    print("what r u doing?")
+else:
+    print("bye")

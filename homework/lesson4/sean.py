8b49ecc0905d8cad3488d83622ea984363e36117
Sadface1000 <seanzsh@outlook.com>
2020-07-07 11:37:41 -0700

    https://github.com/Shuzhengz/pythonintro/compare

---
+++ homework/lesson4/sean.py
@@ -0,0 +1,13 @@
+x = int(input("What is your age?: "))
+if x >= 14: 
+    print("You can join robotics team. ")
+if x >= 16: 
+    print("You can drive and get a job. ")
+if x >= 18: 
+    print("You can go to college. ")
+if x >= 21: 
+    print("you are an adult. ")
+if x >= 35:
+    print("You can become a president. ")
+else:
+    print("You can't do anything. ") 

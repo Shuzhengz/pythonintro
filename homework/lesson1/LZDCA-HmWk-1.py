695f2529103f6df73f7cd4a71ced57c260a6ded9
LZDCA <SL3K4MISC2005@YAHOO.COM>
2020-07-07 11:14:00 -0700

    Li's submission

---
+++ homework/lesson1/LZDCA-HmWk-1.py
@@ -0,0 +1,8 @@
+# HOMEWORK: LESSON 1 : PRINTING AND SIMPLE ARITHMETIC
+#
+print("What is your name?")
+print("My name is Li Zhang")
+
+s = input("What is your age?")
+print("I am " + s + " years old")
+print("Or say I am " + str(int(s) * 12) + " months old")

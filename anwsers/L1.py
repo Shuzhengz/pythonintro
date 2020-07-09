da70fa25b493c5b98e26cf359684d70d6be20ae2
Tom <treez.zhang@gmail.com>
2020-07-09 12:08:44 -0700

    anwsers_1-4

---
+++ anwsers/L1.py
@@ -0,0 +1,4 @@
+age = "14"
+print("Tom Zhang")
+print("Age: " + age)
+print(int(age) * 12)
---
+++ anwsers/L3.py
@@ -0,0 +1,10 @@
+print(float(4.6))#4.6
+print(int(4.6))#4
+print()
+print(int(3.7))#3
+print()
+print(int("4.5"))#error
+print(int("4 and 5"))#error
+print()
+print(float("three point six"))#error
+print(float("three"))#error

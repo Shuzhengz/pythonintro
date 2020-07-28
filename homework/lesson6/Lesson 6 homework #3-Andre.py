659f20e9402893de42f4bb569c3db086a6e77021
AZhang2 <az71677game@gmail.com>
2020-07-27 22:16:39 -0700

    Completed HW for lesson 6-3

--- homework/lesson6/Lesson 6 homework #3-Andre.py
+++ homework/lesson6/Lesson 6 homework #3-Andre.py
@@ -1,51 +1,67 @@
 user1 = "Billy Bob Joe III"
 user2 = "Jhonny Didn't Eat Sugar"
-user3 = "Hacking Shotgun Memer"
+user3 = "Shotgun Hacker"
 userlogin = input("What is your username? ")
 if userlogin == "Billy Bob Joe III":
     print("Welcome Billy.")
-    RetirementSavings = dict({1776 : 999999999})
-    VacationBudget = dict({1917 : 500000})
-    GamingBudget = dict({2012 : 1000000})
-    user1account = {0000 : RetirementSavings , 0o0001 : VacationBudget , 0o0010 : GamingBudget}
-    print(user1account.get(0o0001))
-    user1account = user1account.get(int(input("What is the ID of the account you want to open? ")))
-    accountbalance = user1account.get(int(input("What is your password? ")))
-    if accountbalance == None:
-        while accountbalance == None:
+    Retirement_Savings = dict({1776 : 999999999})
+    Vacation_Budget = dict({1917 : 500000})
+    Gaming_Budget = dict({2012 : 1000000})
+    user1accountnames = {00 : "Retirement_Savings" , 0o01 : "Vacation_Budget" , 0o02 : "Gaming_Budget"}
+    user1accounts = {00 : Retirement_Savings , 0o01 : Vacation_Budget , 0o02 : Gaming_Budget}
+    user1input = input("What is the ID of the account you want to open? ")
+    print("Account: " + user1accountnames.get(int(user1input)))
+    user1accounts = user1accounts.get(int(user1input))
+    accountbalance1 = user1accounts.get(int(input("What is your password? ")))
+    if accountbalance1 == None:
+        while accountbalance1 == None:
             print("The password you entered is incorrect. Please try again.")
-            accountbalance = user1account.get(int(input("What is your password? ")))
-            print(accountbalance)
+            accountbalance1 = user1accounts.get(int(input("What is your password? ")))
+            print("$" + str(accountbalance1))
     else:
-            print(accountbalance)
-
-elif userlogin == "Jhonny Didn't Eat Sugar":
-    SugarSpendingBudget = dict({19217117 : 100000000000000000000})
-    BribePapaBudget = dict({1415 : 999999999})
-    TaxiEscapeBudget = dict({172114 : 999})
-    user2accounts = {0o0011 : SugarSpendingBudget , 0o0100 : BribePapaBudget, 0o0101 : TaxiEscapeBudget}
+        print("$" + str(accountbalance1))
+   
+elif userlogin == "Johnny Didn't Eat Sugar":
+    print("Welcome Johnny.")
+    Stocking_Sugar_Budget = dict({19217117 : 100000000000000000000})
+    Bribe_Papa_Budget = dict({1415 : 999999999})
+    Taxi_Escape_Budget = dict({172114 : 999})
+    user2accountnames = {0o03 : "Stocking_Sugar_Budget" , 0o04 : "Bribe_Papa_Budget" , 0o05 : "Taxi_Escape_Budget"}
+    user2accounts = {0o03 : Stocking_Sugar_Budget , 0o04 : Bribe_Papa_Budget, 0o05 : Taxi_Escape_Budget}
     user2input = input("What is the ID of the account you want to open? ")
-    if accountbalance == None:
-        while accountbalance == None:
+    print("Account: " + user2accountnames.get(int(user2input)))
+    user2accounts = user2accounts.get(int(user2input))
+    accountbalance2 = user2accounts.get(int(input("What is your password? ")))
+    if accountbalance2 == None:
+        while accountbalance2 == None:
             print("The password you entered is incorrect. Please try again.")
-            accountbalance = user2accounts.get(int(input("What is your password? ")))
-            if accountbalance == 999999999:
-                print(accountbalance)
-            else:
-                print("")
+            accountbalance2 = user2accounts.get(int(input("What is your password? ")))
+            print("$" + str(accountbalance2))
     else:
-        user2accounts = user2accounts.get(int(user2input))
-        accountbalance = user2accounts.get(int(input("What is your password? ")))
-        if accountbalance == None:
-            while accountbalance == None:
-                print("The password you entered is incorrect. Please try again.")
-                accountbalance = user2accounts.get(int(input("What is your password? ")))
-                if accountbalance == 999999999:
-                    print(accountbalance)
-                else:
-                    print("")
+        print("$" + str(accountbalance2))
+
+elif userlogin == "Shotgun Hacker":
+    Hacking_For_Sore_Losers_Income = {1112 : 25}
+    Google_Job_Income = {133214 : 121512}
+    New_PCs_For_Development_Team = {131319 : 7500}
+    user3accountnames = {0o06 : "Hacking_For_Sore_Losers_Income" , 0o07 : "Google_Job_Income" , 10 : "New_PCs_For_Development_Team"}
+    user3accounts = {0o06 : Hacking_For_Sore_Losers_Income , 0o07 : Google_Job_Income , 10 : New_PCs_For_Development_Team}
+    user3input = input("What is the ID of the account you want to open? ")
+    print("Account: " + str(user3accountnames.get(int(user3input))))
+    user3accounts = user3accounts.get(int(user3input))
+    accountbalance3 = user3accounts.get(int(input("What is your password? ")))
+    if accountbalance3 == None:
+        while accountbalance3 == None:
+            print("The password you entered is incorrect. Please try again.")
+            accountbalance3 = user3accounts.get(int(input("What is your password? ")))
+            print("$" + str(accountbalance3))
+    else:
+        print("$" + str(accountbalance3))
+
+
 else:
     print("This account does not exist at this time.")
+   
 
 
 

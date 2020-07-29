733289b3e51edfe146f0d86f9a7caa032ea7d028
Sadface1000 <seanzsh@outlook.com>
2020-07-28 19:41:02 -0700

    homework

--- anwsers/L4.py
+++ anwsers/L4.py
@@ -1,15 +1,15 @@
-a = int(input("What is your age? "))
-if a >= 14:
+x = int(input("What is your age? "))
+if x >= 14:
 	print("You could join the Robotics Team.")
-if a >= 16:
+if x >= 16:
 	print("You could drive and get a job.")
-if a >= 18:
+if x >= 18:
 	print("You can attend to college")
-if a == 21:
-	print("Congrats! You are now an adult (which totally sucks).")
-if a > 21:
+if x >= 21:
 	print("You are an adult.")
-if a >= 35:
+if x >= 21:
+	print("You are an adult.")
+if x >= 35:
 	print("You could become the President.")
-if a < 14:
+if x < 14:
 	print("Sorry, you couldn't do anything...")

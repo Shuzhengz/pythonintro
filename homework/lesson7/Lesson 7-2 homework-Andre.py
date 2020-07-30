f695b15f5854e7aa765bece6d12c0c85a23c1914
AZhang2 <az71677game@gmail.com>
2020-07-30 10:54:10 -0700

    Lesson 7 homeword-Andre

---
+++ homework/lesson7/Lesson 7-2 homework-Andre.py
@@ -0,0 +1,22 @@
+print("Please input words or numbers for the array when prompted. To stop, type stop (this code is case sensitive, so please add no uppercase letters).")
+user_input_array = []
+x = 0
+while x != "stop":
+    userinput = input("What word or number would you like to add to the array? ")
+    if userinput == "stop":
+        x = "stop"
+    elif (userinput in user_input_array) is True:
+        print("The word you entered is already in the array.")
+    else: 
+        user_input_array.append(userinput)
+        print("added")
+rev_array = []
+x = len(user_input_array)
+z = 1
+y = user_input_array[x - z]
+while x != z - 1:
+    y = user_input_array[x - z]
+    x = len(user_input_array)
+    rev_array.append(y)
+    z = z + 1
+print("The reversed array is:" + str(rev_array))

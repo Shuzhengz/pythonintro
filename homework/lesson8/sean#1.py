fac39cf68c9b2f7cf9b0bbcac710eab91722aacb
Sadface1000 <seanzsh@outlook.com>
2020-07-31 12:07:52 -0700

    homework

--- homework/lesson8/sean#1.py
+++ homework/lesson8/sean#1.py
@@ -59,6 +59,19 @@ def Lesson_6_2():
     print(store[0] + "'s favorite color is " + store[1] + ". They have " + store[2] + " pets. ")
 print(Lesson_6_2())
 
+#lesson6.3:
+def Lesson_6_3():
+    x = {"1" : 100, "2" : 200, "3" : 300, "4" : 400, "5" : 500, "6" : 0, "7" : 0, "8" : 10000000, "9" : 0, "10" : 0}
+    y = input("Give me your password: ")
+    if y in x:
+        print("You have " + str(x[y]) + " dollars in your account. ")
+        input("Do you want to DELETE your account or your self or both or neither? ")
+        x.pop(y)
+        print("BOTH you and your account have been succesfuly DELETED. Thank you for being here, have a lovely day. ")
+    else:
+        print("WRONG PASSWORD, have a lovely day.")
+print(Lesson_6_3())
+
 #lesson7.1:
 def Lesson_7_1():
     f = []
